07594edeae008f82f07850d6e6781bacfe5a2156
Muhammad Hanif Fajari <105597377+mhaniffajari@users.noreply.github.com>
2022-09-19 23:30:45 +0700

    Add files via upload

---
+++ Practice Case Session 1/Practice_Case_Session_1.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Create a python script which can upload a file from the internet to Google Cloud Storage (Bonus point if
+# you use Docker)
+
+# In[ ]:
+
+
+from google.cloud import storage
+
+
+def authenticate_implicit_with_adc(project_id="your-google-cloud-project-id"):
+    """
+    When interacting with Google Cloud Client libraries, the library can auto-detect the
+    credentials to use.
+
+    // TODO(Developer):
+    //  1. Before running this sample,
+    //  set up ADC as described in https://cloud.google.com/docs/authentication/external/set-up-adc
+    //  2. Replace the project variable.
+    //  3. Make sure that the user account or service account that you are using
+    //  has the required permissions. For this sample, you must have "storage.buckets.list".
+    Args:
+        project_id: The project id of your Google Cloud project.
+    """
+
+    # This snippet demonstrates how to list buckets.
+    # *NOTE*: Replace the client created below with the client required for your application.
+    # Note that the credentials are not specified when constructing the client.
+    # Hence, the client library will look for credentials using ADC.
+    storage_client = storage.Client(project=project_id)
+    buckets = storage_client.list_buckets()
+    print("Buckets:")
+    for bucket in buckets:
+        print(bucket.name)
+    print("Listed all storage buckets.")
+
+
+# In[8]:
+
+
+authenticate_implicit_with_adc(project_id="clever-seat-363006")
+
+
+# In[20]:
+
+
+from google.cloud import storage
+import os
+
+def upload_blob_from_memory(bucket_name, contents, destination_blob_name):
+    """Uploads a file to the bucket."""
+
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+
+    # The contents to upload to the file
+    # contents = "these are my contents"
+
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client(project="clever-seat-363006")
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_string(contents)
+
+    print(
+        f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
+    )
+
+
+# In[18]:
+
+
+file_path = 'C:/Users/Muhammad Hanif/Desktop/Data Science/Data Engineer/test.txt'
+
+
+# In[21]:
+
+
+upload_blob_from_memory('fellowship-777',file_path, 'test.txt')
+
